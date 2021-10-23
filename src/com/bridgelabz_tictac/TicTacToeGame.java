66a6c3e6a935bd19ce8e13d658471626bdaf0669
Basila123 <basilanoushad92@gmail.com>
2021-10-23 23:59:53 +0530

    Added Uc3: created a game board

--- src/com/bridgelabz_tictac/TicTacToeGame.java
+++ src/com/bridgelabz_tictac/TicTacToeGame.java
@@ -13,6 +13,7 @@ public class TicTacToeGame {
         // calling the method createBoard
         createBoard();
         userInput();
+        showBoard();
     }
 
     // Method to create an empty character array
@@ -47,6 +48,21 @@ public class TicTacToeGame {
 
         //play.close();
 
+    }
+    public static void showBoard(){
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[1] + " | "
+                + board[2] + " | " + board[3]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[4] + " | "
+                + board[5] + " | " + board[6]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[7] + " | "
+                + board[8] + " | " + board[9]
+                + " |");
+        System.out.println("|---|---|---|");
     }
 }
 
